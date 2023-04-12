da44b623487b614b988e4de3f9d941d4a9b033bd
zanghan <zanghan@xd.com>
2023-04-12 13:23:30 +0800

    none

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ git reset --hard origin/master
 git pull
 
 docker build -t chat_vue_img .
-docker rm -f chat_vue
-docker run -itd -p 5173:5173 --name chat_vue -v "$pwd":/app chat_vue_img
+
+docker run --rm -itd -p 5173:5173 --name chat_vue -v $(pwd):/app chat_vue_img
